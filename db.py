4190a18e3ab3e3efbf278b119709456e07bd825c
Theriley106 <christopherlambert106@gmail.com>
2018-05-13 14:49:30 -0400

    Updated db.json

--- db.py
+++ db.py
@@ -1,18 +1,5 @@
-import sqlite3
-conn = sqlite3.connect('userDB.db')
+import json
 
-c = conn.cursor()
+DATABASE = json.load(open("database.json"))
 
-# Create table
-c.execute('''CREATE TABLE users
-             (date text, trans text, symbol text, qty real, price real)''')
-
-# Insert a row of data
-c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
-
-# Save (commit) the changes
-conn.commit()
-
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
-conn.close()
+print DATABASE
