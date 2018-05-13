8bee4f198dec632e9502f62b96f6e51263e9f3b4
Theriley106 <christopherlambert106@gmail.com>
2018-05-13 15:18:46 -0400

    Added to follow

--- db.py
+++ db.py
@@ -14,17 +14,33 @@ clientID = random.choice(idList)
 DATABASE_FILE = "database.json"
 DATABASE = json.load(open(DATABASE_FILE))
 
+def resetDB():
+	os.system("mv {} {}".format(DATABASE_FILE, "backup.json"))
+	os.system("mv {} {}".format("dbTemplate.json", DATABASE_FILE))
+
 def updateDB():
 	os.system("mv {} {}".format(DATABASE_FILE, "backup.json"))
 	with open(DATABASE_FILE, 'w') as f:
 		json.dump(DATABASE, f)
 	print("Updated")
 
+def updateUser(userInfo):
+	for val in DATABASE['users']:
+		if val['id'] == userInfo['id']:
+			DATABASE['users'].remove(val)
+			DATABASE['users'].append(userInfo)
+			break
+	DATABASE["userIDs"].append(userInfo['id'])
+
 def addFollower(userInfo):
+	if userInfo['id'] in DATABASE["userIDs"]:
+		updateUser(userInfo)
+	else:
+		DATABASE["userIDs"].append(userInfo['id'])
+		DATABASE["users"].append(userInfo)
 	if userInfo['id'] not in DATABASE["followers"]:
 		DATABASE["followers"].append(userInfo['id'])
-		DATABASE["users"].append(userInfo)
-		updateDB()
+	updateDB()
 
 
 
