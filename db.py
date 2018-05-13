fde6903a84b3cfa6c73a2594934840a62729598b
Theriley106 <christopherlambert106@gmail.com>
2018-05-13 15:00:13 -0400

    Added addFollower(

--- db.py
+++ db.py
@@ -2,4 +2,9 @@ import json
 
 DATABASE = json.load(open("database.json"))
 
+def addFollower(userInfo):
+	if userInfo['id'] not in DATABASE["followers"]:
+		DATABASE["followers"].append(userInfo['id'])
+		DATABASE["users"].append(userInfo)
+
 print DATABASE
