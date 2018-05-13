482369ad9a76f370ade01b85803e3aea8a211d41
Theriley106 <christopherlambert106@gmail.com>
2018-05-13 15:05:41 -0400

    Added correct database interaction wtih adduser

--- db.py
+++ db.py
@@ -1,10 +1,35 @@
 import json
+import requests
+import random
+import os
 
-DATABASE = json.load(open("database.json"))
+idList = []
+for val in open("listOfClientIDs.txt").read().split("\n"):
+	if len(val) > 5:
+		idList.append(val)
+if len(idList) == 0:
+	raise Exception("No Client IDs")
+clientID = random.choice(idList)
+
+DATABASE_FILE = "database.json"
+DATABASE = json.load(open(DATABASE_FILE))
+
+def updateDB():
+	os.system("mv {} {}".format(DATABASE_FILE, "backup.json"))
+	with open(DATABASE_FILE, 'w') as f:
+		json.dump(DATABASE, f)
+	print("Updated")
 
 def addFollower(userInfo):
 	if userInfo['id'] not in DATABASE["followers"]:
 		DATABASE["followers"].append(userInfo['id'])
 		DATABASE["users"].append(userInfo)
+		updateDB()
 
-print DATABASE
+
+
+if __name__ == '__main__':
+	url = "https://api-v2.soundcloud.com/users/38122545/followers?&limit=200&client_id={}".format(clientID)
+	res = requests.get(url)
+	for val in res.json()['collection']:
+		addFollower(val)
