d3ab3357f151140c8b5392fe322cdddb2fa6fd09
Theriley106 <christopherlambert106@gmail.com>
2018-05-20 09:47:46 -0400

    Added data visualization showing like to follow ratio

--- dataVisualizations/app.py
+++ dataVisualizations/app.py
@@ -3,35 +3,24 @@ import json
 
 app = Flask(__name__, static_url_path='/static')
 
-listOfFollowers = json.load(open("static/datasets/database.json"))["followers"]
-print listOfFollowers
 @app.route('/', methods=['GET'])
 def index():
 	return render_template("index.html")
 
 @app.route('/viz1', methods=['GET'])
 def testPage():
+	likeCount = []
 	followInfo = []
+	tDict = []
 	listOfFollowers = json.load(open("static/datasets/database.json"))["followers"]
 	for val in json.load(open("static/datasets/userInfo.json")):
 		if val['id'] in listOfFollowers:
 			followInfo.append(val)
-	return jsonify(followInfo)
-	return render_template("viz1.html")
-
-'''@app.route('/Jan19/', methods=['GET'])
-def Jan19():
-	import Jan17
-	DATABASE=Jan17.getDatabase()
-	for data in DATABASE:
-		if data['Nootropics']["Sentiment"] == 0 or data['StackAdvice']["Sentiment"] == 0:
-			DATABASE.remove(data)
-	DATABASE = sorted(DATABASE, key=lambda k: k['StackAdvice']['Occurances'])
-	date = sys._getframe().f_code.co_name
-	if xUtilities.checkForScreenshot(date) == False:
-		threading.Thread(target=xUtilities.saveScreenshot, args=(date,)).start()
-		return "Getting Screenshot"
-	return render_template("Jan19.html", DATABASE=DATABASE[::-1][:10])'''
+	for val in followInfo:
+		ratio = (float(val['likes_count']) / float(val['followings_count']))
+		tDict.append({"Ratio": ratio, "ID": val['id']})
+	DATABASE = sorted(tDict, key=lambda k: k['Ratio'])
+	return render_template("viz1.html", DATABASE=DATABASE)
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=5000)
