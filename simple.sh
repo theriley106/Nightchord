4f26ac3b3f64d0cefd7f9994a9f2de2e389b254f
theriley106 <christopherlambert106@gmail.com>
2019-09-02 16:03:16 -0700

    Added simple setup script

--- simple.sh
+++ simple.sh
@@ -1,10 +1,16 @@
-if [ ! -f .installCompleted ]; then
-	pip install -r simple_requirements.txt
-    if python -c "import moviepy;import requests;import bs4;" ; then
-   		touch .installCompleted
+if which sox; then
+    if [ ! -f .installCompleted ]; then
+		pip install -r simple_requirements.txt
+	    if python -c "import moviepy;import requests;import bs4;" ; then
+	   		touch .installCompleted
+		fi
+		else
+		    echo "Dependencies already installed - starting nightcore generator"
 	fi
-	else
-	    echo "Dependencies already installed - starting nightcore generator"
+
+	python simple.py
+else
+    echo 'SOX IS NOT INSTALLED'
+    echo "Download sox and rerun simple.sh"
 fi
 
-python simple.py
